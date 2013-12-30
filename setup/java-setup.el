01bb1e79d8a0ce6d710a7aef51c9d59fe7957087
Naoto Yokoyama <builtinnya@gmail.com>
2013-12-30 13:45:16 +0900

    Set up indentation for Java

--- setup/java-setup.el
+++ setup/java-setup.el
@@ -4,4 +4,16 @@
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 
+(defun my-indent-setup ()
+  (c-set-offset 'arglist-intro '++)
+  (c-set-offset 'arglist-cont '++)
+  (c-set-offset 'arglist-cont-nonempty '++)
+  (c-set-offset 'arglist-close '++))
+
+(add-hook 'java-mode-hook 'my-indent-setup)
+
+(add-hook 'c-initialization-hook
+          (lambda ()
+            (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)))
+
 (provide 'java-setup)
