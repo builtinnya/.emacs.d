c5ef98db07a110040d31b8176db4fcd4c16514a8
Naoto Yokoyama <builtinnya@gmail.com>
2013-12-30 13:42:52 +0900

    Added a personal dictionary for Flyspell

--- setup/flyspell-setup.el
+++ setup/flyspell-setup.el
@@ -1,9 +1,17 @@
-;;; Set up flyspell-mode, an on-the-fly spell checking mode
+;;; Set up Flyspell, an on-the-fly spell checking mode
 
 ;; Flyspell-mode does spell-checking on the fly as you type
 (require 'flyspell)
+
 (setq ispell-program-name "aspell"
       ispell-extra-args '("--sug-mode=ultra"))
+
+;; Do not emit messages when checking words for improving performance
+(setq flyspell-issue-message-flag nil)
+
+;; Set the personal dictionary path
+(setq ispell-personal-dictionary
+      (expand-file-name "aspell.pws" preferences-dir))
 
 ;; Enable flyspell in text-mode
 (add-hook 'text-mode-hook 'flyspell-mode)
