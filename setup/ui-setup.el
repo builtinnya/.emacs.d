34ca0edbb15d9c55c3cb4c536d1f851a004f67bf
Naoto Yokoyama <builtinnya@gmail.com>
2014-01-10 23:33:37 +0900

    Use powerline instead of emacs-powerline

--- setup/ui-setup.el
+++ setup/ui-setup.el
@@ -25,9 +25,79 @@
 (load-theme 'soft-morning t)
 
 ;; Make mode line look better
-(add-vendor-to-load-path "emacs-powerline")
+(add-vendor-to-load-path "powerline")
 (require 'powerline)
 
+(set-face-attribute 'mode-line nil
+                    :background "OliveDrab3"
+                    :box nil)
+
+(set-face-attribute 'mode-line-inactive nil
+                    :box nil)
+
+(set-face-attribute 'powerline-active1 nil
+                    :foreground "white"
+                    :background "grey22")
+
+(set-face-attribute 'powerline-active2 nil
+                    :foreground "white"
+                    :background "grey40")
+
+;;;###autoload
+(defun powerline-my-theme ()
+  "Set up my mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((mode-line 'mode-line)
+                          (face1 'powerline-active1)
+                          (face2 'powerline-active2)
+                          (separator-left
+                           (intern
+                            (format "powerline-%s-%s"
+                                    powerline-default-separator
+                                    (car powerline-default-separator-dir))))
+                          (separator-right
+                           (intern
+                            (format "powerline-%s-%s"
+                                    powerline-default-separator
+                                    (cdr powerline-default-separator-dir))))
+                          (lhs
+                           (list (powerline-raw "%*" nil 'l)
+                                 (powerline-buffer-size nil 'l)
+                                 (powerline-raw mode-line-mule-info nil 'l)
+                                 (powerline-buffer-id nil 'l)
+                                 (when (and (boundp 'which-func-mode)
+                                            which-func-mode)
+                                   (powerline-raw which-func-format nil 'l))
+                                 (powerline-raw " ")
+                                 (funcall separator-left nil face1)
+                                 (when (boundp 'erc-modified-channels-object)
+                                   (powerline-raw erc-modified-channels-object
+                                                  face1 'l))
+                                 (powerline-major-mode face1 'l)
+                                 (powerline-process face1)
+                                 (powerline-minor-modes face1 'l)
+                                 (powerline-narrow face1 'l)
+                                 (powerline-raw " " face1)
+                                 (funcall separator-left face1 face2)
+                                 (powerline-vc face2 'r)))
+                          (rhs
+                           (list (powerline-raw global-mode-string face2 'r)
+                                 (funcall separator-right face2 face1)
+                                 (powerline-raw "%4l" face1 'l)
+                                 (powerline-raw ":" face1 'l)
+                                 (powerline-raw "%3c" face1 'r)
+                                 (funcall separator-right face1 nil)
+                                 (powerline-raw " ")
+                                 (powerline-raw "%6p" nil 'r)
+                                 (powerline-hud face2 face1))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+;; Load my powerline theme
+(powerline-my-theme)
 
 ;; Unclutter the mode line
 (require 'diminish)
