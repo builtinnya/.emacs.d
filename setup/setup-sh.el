900b7c78fc32e020b9e496ed11c9c3c3b64de40f
Naoto Yokoyama <builtinnya@gmail.com>
2014-03-02 18:38:29 +0900

    Open .zsh with sh-mode

--- setup/setup-sh.el
+++ setup/setup-sh.el
@@ -3,5 +3,6 @@
 ;; Use `sh-mode' for editing the following dot-files.
 (add-to-list 'auto-mode-alist '("/_zshrc\\'" . sh-mode))
 (add-to-list 'auto-mode-alist '("/_zshenv\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
 
 (provide 'setup-sh)
